4488b92c383e1c26010907f0a37cbaf294ce50ef
Yongmin Jang <althea_@naver.com>
2023-03-08 21:04:38 +0900

    Add logout code

--- core/src/main/java/com/joolove/core/security/jwt/repository/RefreshTokenRepository.java
+++ core/src/main/java/com/joolove/core/security/jwt/repository/RefreshTokenRepository.java
@@ -4,7 +4,10 @@ import com.joolove.core.domain.auth.RefreshToken;
 import com.joolove.core.domain.member.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.Ref;
 import java.util.Optional;
@@ -15,6 +18,13 @@ public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long
 
     Optional<RefreshToken> findByUser(User user);
 
-    @Modifying
-    int deleteByUser(User user);
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    @Query("delete from RefreshToken r where r.user = :user")
+    int deleteByUser(@Param("user") User user);
+
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    @Query("delete from RefreshToken r where r.token = :token")
+    int deleteByToken(@Param("token") String token);
 }
