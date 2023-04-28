53ae199cfec00daaa9fb2a181db851191f0d74b7
Yongmin Jang <althea_@naver.com>
2023-04-28 16:57:34 +0900

    Update scraping code with GoodsDetails entity

---
+++ core/src/main/java/com/joolove/core/security/service/GoodsDetailsService.java
@@ -0,0 +1,21 @@
+package com.joolove.core.security.service;
+
+import com.joolove.core.domain.product.Goods;
+import com.joolove.core.domain.product.GoodsDetails;
+import com.joolove.core.repository.GoodsDetailsRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class GoodsDetailsService {
+
+    private final GoodsDetailsRepository goodsDetailsRepository;
+
+    @Transactional
+    public void addGoodsDetails(GoodsDetails goodsDetails) {
+        goodsDetailsRepository.save(goodsDetails);
+    }
+}
