0fda0afd8782c1f5b67a078c4a77aaaabb43f474
JAlthea <althea_@naver.com>
2023-07-05 19:41:02 +0900

    Chore setting

--- core/src/main/java/com/joolove/core/CoreApplication.java
+++ core/src/main/java/com/joolove/core/CoreApplication.java
@@ -3,16 +3,25 @@ package com.joolove.core;
 import com.joolove.core.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @EnableConfigurationProperties(AppProperties.class)
 @SpringBootApplication
-public class CoreApplication {
+public class CoreApplication extends SpringBootServletInitializer {
+
+	private static Class<CoreApplication> applicationClass = CoreApplication.class;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoreApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(applicationClass);
 	}
 
 }
