0bcc57fd109da19d06aec420969e76f537ba972f
JAlthea <althea_@naver.com>
2023-07-12 19:47:53 +0900

    Modify previous version check

--- core/scripts/deploy.sh
+++ core/scripts/deploy.sh
@@ -33,4 +33,4 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-nohup java -jar -Dspring.config.location=/home/ec2-user/app/application-live.yml $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar -Dspring.config.location=/home/ec2-user/app/application-live1.yml $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
